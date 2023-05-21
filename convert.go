8a0447eb1b0d6c843833adebe00062bcd24760ad
Nathan Brock <nathan@theapimethod.com>
2023-05-21 15:57:11 +0100

    Add initial converter

--- convert.go
+++ convert.go
@@ -22,18 +22,22 @@ type Filter struct {
 }
 
 func main() {
-	var filePath string
-	flag.StringVar(&filePath, "file", "", "path to maxmind csv file")
+	var inputFile string
+	flag.StringVar(&inputFile, "input_csv", "", "path to maxmind csv file")
+	var dir string
+	flag.StringVar(&dir, "output_dir", "./", "output directory for resulting files")
+	var filename string
+	flag.StringVar(&filename, "output_file", "ip_block_list", "filename of the resulting files")
+	var name string
+	flag.StringVar(&name, "list_name", "", "name used in blocklist")
+	var desc string
+	flag.StringVar(&desc, "list_desc", "", "description used in blocklist")
 	var filterCountryID string
 	flag.StringVar(&filterCountryID, "countryid", "", "filter by maxmind country id")
 
 	flag.Parse()
 
-	if filePath == "" {
-		log.Fatal("-file={filepath} flag required")
-	}
-
-	file, err := os.Open(filePath)
+	file, err := os.Open(inputFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,18 +51,32 @@ func main() {
 		log.Fatal(err)
 	}
 
-	lists := buildBlockList(addrs)
-
-	for i, list := range lists {
-		err := writeJSONToFile(list, fmt.Sprintf("ip_block_list_%s_%d.lsrules", filterCountryID, i))
-		if err != nil {
-			log.Fatal(err)
-		}
+	lists := buildBlockList(name, desc, addrs)
+	err = writeListsToFile(dir, filename, lists)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
+// writeListsToFile takes a slice of block lists and writes them to the given directory
+func writeListsToFile(dir, filename string, lists []BlockList) error {
+	for i, list := range lists {
+		suf := ""
+		if len(lists) > 1 {
+			suf = fmt.Sprintf("_%d", i+1)
+		}
+
+		err := writeJSONToFile(list, fmt.Sprintf("%s/%s%s.lsrules", dir, filename, suf))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 // buildBlockList creates one or more LittleSnitch block list structs for the given slice of IP addresses
-func buildBlockList(ips []net.IPNet) []BlockList {
+func buildBlockList(name, desc string, ips []net.IPNet) []BlockList {
 	sets := make([]BlockList, 0)
 
 	maxSize := 200000
@@ -77,8 +95,8 @@ func buildBlockList(ips []net.IPNet) []BlockList {
 		}
 
 		sets = append(sets, BlockList{
-			Description:         "Block IPs",
-			Name:                "Block IPs",
+			Description:         name,
+			Name:                desc,
 			DeniedRemoteAddress: addrs,
 		})
 	}
